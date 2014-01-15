031dfb955eac9064b021e099d047ce0adfd04d3c
B Kleinen <kleinen@infrastructure.de>
2014-01-15 20:25:52 +0100

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Songbook::Application.config.secret_key_base = 'ad9341cf6e2711f3e4c09228faa6580c6b2fe845df6464325d105ba8bb3cc8063ca7d7c1c9f8a5159483a564cf91e9fb469f39d8d05cbdfb0e4ace9f44c26d1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Songbook::Application.config.session_store :cookie_store, key: '_songbook_session'
