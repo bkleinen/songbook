ee309db400859924dac794fe5f6e7c414a7802af
B Kleinen <kleinen@infrastructure.de>
2014-01-17 14:59:33 +0100

    committing failing test to show off travis

--- spec/requests/songs_landing_spec.rb
+++ spec/requests/songs_landing_spec.rb
@@ -2,13 +2,21 @@ require 'spec_helper'
 
 describe "Songs" do
   describe "landing page" do
-    before(:each) do
-      create(:father_and_son)
-      create(:common)
-    end
-    it "shows the songs in the correct order" do
+    context "without songs" do
+      it "shows a decent message" do
         visit root_path
-        expect(page).to have_content 'Latest Song: Common People by Pulp'
+        expect(page).to have_content 'No songs so far.'
+      end
+    end
+    context "with two songs" do
+      before(:each) do
+        create(:father_and_son)
+        create(:common)
+      end
+      it "shows the songs in the correct order" do
+          visit root_path
+          expect(page).to have_content 'Latest Song: Common People by Pulp'
+      end
     end
   end
 end
