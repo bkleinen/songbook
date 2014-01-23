afe5fc4e048f0fabf24877b25a6e9910272c7368
B Kleinen <kleinen@infrastructure.de>
2014-01-23 02:08:49 +0100

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140122234429) do
+ActiveRecord::Schema.define(version: 20140123010720) do
 
   create_table "artists", force: true do |t|
     t.string   "first"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140122234429) do
     t.integer  "stars"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "artist_id"
   end
 
 end
